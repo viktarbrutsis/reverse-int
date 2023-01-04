6a3092790c05b39512cf0a2887e87e75ab733e42
Viktar Brutsis <viktarbrutsis@gmail.com>
2023-01-04 20:41:57 +0300

    feat: add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
-}
+    let newNum = parseInt(n.toString().split('').reverse().join(''));
+    return newNum;
+};
+// function reverseInt(num) {
+//     let newNum = parseInt(num.toString().split('').reverse().join(''));
+//     // return newNum;
+//     console.log(newNum);
+// }
+
+
+// reverseInt(-1592);
